d87d5594176db13c7f9a12d5a1cc462dc6028c91
Paul <easylive1989@gmail.com>
2023-05-14 00:14:44 +0800

    extract dominoSize

--- lib/game/domino.dart
+++ lib/game/domino.dart
@@ -2,10 +2,12 @@ import 'package:flame/components.dart';
 import 'package:flutter_tetris/game/tetris_game.dart';
 
 class Domino extends RectangleComponent with HasGameRef<TetrisGame> {
+  static const double _dominoSize = 20.0;
+
   double _totalDelta = 0;
   bool _isStop = false;
 
-  Domino() : super(size: Vector2(20, 20));
+  Domino() : super(size: Vector2(_dominoSize, _dominoSize));
 
   @override
   void update(double dt) {
@@ -14,7 +16,7 @@ class Domino extends RectangleComponent with HasGameRef<TetrisGame> {
     if (_isStop) return;
 
     var floor = _totalDelta.floor();
-    position.y = 20.0 * floor;
+    position.y = _dominoSize * floor;
     if (gameRef.dominoSlots[floor + 1][0]) {
       gameRef.dominoSlots[floor][0] = true;
       if (gameRef.dominoSlots[0][0]) {
