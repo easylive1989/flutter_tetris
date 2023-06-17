739e02bb07dcb4c5eebd489caba027fe266cda8d
Paul <easylive1989@gmail.com>
2023-06-17 09:30:01 +0800

    refactor domino_board

--- lib/game/domino_board.dart
+++ lib/game/domino_board.dart
@@ -3,6 +3,8 @@ import 'package:flutter_tetris/game/domino.dart';
 import 'package:flutter_tetris/game/tetris_game.dart';
 
 class DominoBoard extends Component with HasGameRef<TetrisGame> {
+  static const int _column = 10;
+  static const int _row = 20;
 
   @override
   Future onLoad() async {
@@ -12,28 +14,40 @@ class DominoBoard extends Component with HasGameRef<TetrisGame> {
 
   int _eliminateCount = 0;
 
-  int get dominoCount => children.length + _eliminateCount * 10;
+  int get _lastRow => _row - 1;
 
-  bool get isDominoReachTop => children.whereType<Domino>().any((domino) => domino.floor == 0 && domino.isStop);
+  int get dominoCount => children.length + _eliminateCount * _column;
+
+  bool get isDominoReachTop => children
+      .whereType<Domino>()
+      .any((domino) => domino.floor == 0 && domino.isStop);
 
   @override
   void update(double dt) {
     super.update(dt);
     var lastDomino = children.last as Domino;
 
-    if (lastDomino.floor == 19 || _isReachOtherDomino(lastDomino)) {
+    if (lastDomino.floor == _lastRow || _isReachOtherDomino(lastDomino)) {
       lastDomino.stop();
       var dominoes = _sameRowDominoes(lastDomino);
-      if (dominoes.length == 10) {
-        removeAll(dominoes);
-        _eliminateCount++;
+      if (dominoes.length == _column) {
+        _eliminate(dominoes);
       }
       add(Domino());
     }
   }
 
-  Iterable<Domino> _sameRowDominoes(Domino lastDomino) => children.whereType<Domino>().where((domino) => domino.floor == lastDomino.floor && domino.isStop);
+  void _eliminate(Iterable<Domino> dominoes) {
+    removeAll(dominoes);
+    _eliminateCount++;
+  }
 
-  bool _isReachOtherDomino(Domino lastDomino) => children.whereType<Domino>().any((domino) => domino.floor == lastDomino.floor + 1 && domino.column == lastDomino.column );
+  Iterable<Domino> _sameRowDominoes(Domino lastDomino) => children
+      .whereType<Domino>()
+      .where((domino) => domino.floor == lastDomino.floor && domino.isStop);
 
+  bool _isReachOtherDomino(Domino lastDomino) =>
+      children.whereType<Domino>().any((domino) =>
+          domino.floor == lastDomino.floor + 1 &&
+          domino.column == lastDomino.column);
 }
